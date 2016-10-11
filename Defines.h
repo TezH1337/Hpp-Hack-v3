3873016f465e6d917e4cf55bfc9f5e68423167fe
kilabeez <plzacesteam@hotmail.com>
2016-10-11 16:47:52 +1000

    Update Defines.h

--- Defines.h
+++ Defines.h
@@ -1,7 +1,7 @@
-#define HW_DLL					"hw.dll"
-#define SW_DLL					"sw.dll"
-#define CLIENT_DLL				"client.dll"
-#define GAMEUI_DLL				"GameUI.dll"
+#define HW_DLL				"hw.dll"
+#define SW_DLL				"sw.dll"
+#define CLIENT_DLL			"client.dll"
+#define GAMEUI_DLL			"GameUI.dll"
 
 #define CLIENT_PATTERN			"ScreenFade"
 #define STUDIO_PATTERN			"Couldn't get client .dll studio model rendering interface."
@@ -29,16 +29,16 @@
 
 #define USERMSG_ERROR			"Error #0012. Couldn't find %s.\nNote: Cheat can't support this game.\nSupported Games: Half-Life, Counter-Strike, Counter-Strike: Condition Zero, Counter-Strike: Condition Zero Deleted Scenes."
 
-#define VERSION					"3.0 [dev]\n"
-#define AUTHOR					"kilabeez\n"
+#define VERSION				"3.0 [dev]\n"
+#define AUTHOR				"kilabeez\n"
 
 #define LUCIDA_CONSOLE			"Lucida Console"
 
-#define MAIN					"Main"
-#define FUNCTIONS				"Functions"
-#define mESP					"ESP"
+#define MAIN				"Main"
+#define FUNCTIONS			"Functions"
+#define mESP				"ESP"
 
-#define MAIN_PATH				"\\Settings\\Main.ini"
+#define MAIN_PATH			"\\Settings\\Main.ini"
 #define VISUALS_PATH			"\\Settings\\Visuals.ini"
 #define FUNCTIONS_PATH			"\\Settings\\Functions.ini"
 
@@ -47,25 +47,25 @@
 
 #define FUNCTIONS_ESP			"|ESP"
 
-#define ESP_ENABLE				"|ESP.Enable"
-#define ESP_PLAYER				"|ESP.Player"
-#define ESP_PLAYER_VISIBLE_ONLY	"|ESP.Player.VisibleOnly"
+#define ESP_ENABLE			"|ESP.Enable"
+#define ESP_PLAYER			"|ESP.Player"
+#define ESP_PLAYER_VISIBLE_ONLY		"|ESP.Player.VisibleOnly"
 #define ESP_VISIBLE_CHECK		"|ESP.Player.VisibleCheck"
 #define ESP_PLAYER_BOX			"|ESP.Player.Box"
 #define ESP_PLAYER_BOX_LW		"|ESP.Player.Box.LineWidth"
-#define ESP_PLAYER_BOX_T_VIS	"|ESP.Player.Box.T.Vis"
-#define ESP_PLAYER_BOX_T_HIDE	"|ESP.Player.Box.T.Hide"
-#define ESP_PLAYER_BOX_CT_VIS	"|ESP.Player.Box.CT.Vis"
-#define ESP_PLAYER_BOX_CT_HIDE	"|ESP.Player.Box.CT.Hide"
+#define ESP_PLAYER_BOX_T_VIS		"|ESP.Player.Box.T.Vis"
+#define ESP_PLAYER_BOX_T_HIDE		"|ESP.Player.Box.T.Hide"
+#define ESP_PLAYER_BOX_CT_VIS		"|ESP.Player.Box.CT.Vis"
+#define ESP_PLAYER_BOX_CT_HIDE		"|ESP.Player.Box.CT.Hide"
 
 #define TERRORIST_UMSG			"TERRORIST"
-#define CT_UMSG					"CT"
+#define CT_UMSG				"CT"
 
-#define RESET_HUD				"ResetHUD"
-#define SET_FOV					"SetFOV"
-#define TEAM_INFO				"TeamInfo"
-#define CUR_WEAPON				"CurWeapon"
-#define DEATH_MSG				"DeathMsg"
+#define RESET_HUD			"ResetHUD"
+#define SET_FOV				"SetFOV"
+#define TEAM_INFO			"TeamInfo"
+#define CUR_WEAPON			"CurWeapon"
+#define DEATH_MSG			"DeathMsg"
 
 #define CompareMemory(Buff1, Buff2, Size) __comparemem((const UCHAR *)Buff1, (const UCHAR *)Buff2, (UINT)Size)
 #define FindMemoryClone(Start, End, Clone, Size) __findmemoryclone((const ULONG)Start, (const ULONG)End, (const ULONG)Clone, (UINT)Size)
@@ -74,7 +74,7 @@
 #define VectorTransform(a,b,c){(c)[0]=DotProduct((a),(b)[0])+(b)[0][3];(c)[1]=DotProduct((a),(b)[1])+(b)[1][3];(c)[2]=DotProduct((a),(b)[2])+(b)[2][3];}
 
 #define TERRORIST				1
-#define CT						2
+#define CT					2
 #define SPECTATOR				0
 
 #define FL_NONE					0
@@ -88,44 +88,44 @@
 
 #define	K_POINT					46
 #define K_COMMA					44
-#define K_0						48
-#define K_1						49
-#define K_2						50
-#define K_3						51
-#define K_4						52
-#define K_5						53
-#define K_6						54
-#define K_7						55
-#define K_8						56
-#define K_9						57
-#define K_BRACKET_LEFT			91
-#define K_BRACKET_RIGHT			93
-#define K_A						97
-#define K_B						98
-#define K_C						99
-#define K_D						100
-#define K_E						101
-#define K_F						102
-#define K_G						103
-#define K_H						104
-#define K_I						105
-#define K_J						106
-#define K_K						107
-#define K_L						108
-#define K_M						109
-#define K_N						110
-#define K_O						111
-#define K_P						112
-#define K_Q						113
-#define K_R						114
-#define K_S						115
-#define K_T						116
-#define K_U						117
-#define K_V						118
-#define K_W						119
-#define K_X						120
-#define K_Y						121
-#define K_Z						122
+#define K_0					48
+#define K_1					49
+#define K_2					50
+#define K_3					51
+#define K_4					52
+#define K_5					53
+#define K_6					54
+#define K_7					55
+#define K_8					56
+#define K_9					57
+#define K_BRACKET_LEFT				91
+#define K_BRACKET_RIGHT				93
+#define K_A					97
+#define K_B					98
+#define K_C					99
+#define K_D					100
+#define K_E					101
+#define K_F					102
+#define K_G					103
+#define K_H					104
+#define K_I					105
+#define K_J					106
+#define K_K					107
+#define K_L					108
+#define K_M					109
+#define K_N					110
+#define K_O					111
+#define K_P					112
+#define K_Q					113
+#define K_R					114
+#define K_S					115
+#define K_T					116
+#define K_U					117
+#define K_V					118
+#define K_W					119
+#define K_X					120
+#define K_Y					121
+#define K_Z					122
 
 #define PARS_STRING(key_1, key_2, to_convent) {\
 	if (!lstrcmpA(Key, key_1) || !lstrcmpA(Key, key_2))\
