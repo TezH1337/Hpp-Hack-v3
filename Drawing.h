268718cbe9186cc6716c75ad4dafcb53c256de68
kilabeez <plzacesteam@hotmail.com>
2016-10-11 18:08:40 +1000

    Updated Drawing

--- Drawing.h
+++ Drawing.h
@@ -7,16 +7,16 @@ namespace Engine
 	class Drawing
 	{
 	public:
-		void FillArea ( int x, int y, int w, int h, BYTE r, BYTE g, BYTE b, BYTE a );
-		void FillArea2 ( int x, int y, int w, int h, BYTE r, BYTE g, BYTE b, BYTE a );
+		void FillArea ( int x, int y, int w, int h, BYTE r, BYTE g, BYTE b, BYTE a, BYTE style );
 
-		void DrawBox ( int x, int y, int w, int h, int linewidth, BYTE r, BYTE g, BYTE b, BYTE a );
-		void DrawBox2 ( int x, int y, int w, int h, int linewidth, BYTE r, BYTE g, BYTE b, BYTE a );
+		void DrawBox ( int x, int y, int w, int h, int linewidth, BYTE r, BYTE g, BYTE b, BYTE a , BYTE style );
 
-		void DrawShadowBox ( int x, int y, int w, int h, BYTE r, BYTE g, BYTE b, BYTE a );
+		void DrawShadowBox ( int x, int y, int w, int h, int linewidth, BYTE r, BYTE g, BYTE b, BYTE a, BYTE style );
 
-		void DrawLine ( int x1, int y1, int x2, int y2, float lw, BYTE r, BYTE g, BYTE b );
-		void DrawVectorLine ( float *flSrc, float *flDestination, float lw, BYTE r, BYTE g, BYTE b );
+		void DrawLine ( int x1, int y1, int x2, int y2, float linewidth, BYTE r, BYTE g, BYTE b, BYTE style );
+		void DrawVectorLine ( float *flSrc, float *flDestination, float linewidth, BYTE r, BYTE g, BYTE b, BYTE style );
+
+		void Draw3DBox ( int Index, float lw, BYTE r, BYTE g, BYTE b, BYTE style );
 	};
 
 	extern Drawing* g_Drawing;
